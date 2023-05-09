6a5e2a77ee346829f02ef722217385839283b0b5
Abraham Pc <xabr12@gmail.com>
2023-05-08 18:29:02 -0600

    tareas y practicas updt

--- src/components/Lab.jsx
+++ src/components/Lab.jsx
@@ -1,36 +1,90 @@
-import React from 'react';
- const Lab = () => {
-    return ( 
-        <div className='container text-white bg-slate-600 min-w-full px-12 pb-32'>
-            <h2 className='pt-3 text-3xl'>Prácticas Laboratorio</h2>
-            <h5 className='pt-3 text-lg'>Luis Abraham Meléndez Hernández</h5>
-            <h5 className='py-3 mb-4 text-lg'>Ramón Alberto Romero Salazar</h5>
-            <div className='lg:mx-20'>
-                <table className="table table-dark table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Link</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">Páctica 1</th>
-                            <td><a className='btn btn-error lg:btn-sm' href="practicas/Practica1.docx">Descargar</a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Páctica 2</th>
-                            <td><a className='btn btn-error lg:btn-sm' href="practicas/Practica2.docx">Descargar</a></td>
-                        </tr>   
-                        <tr>
-                            <th scope="row">Páctica 5</th>
-                            <td><a className='btn btn-error lg:btn-sm' href="practicas/Practica5.docx">Descargar</a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-     );
- }
-  
- export default Lab;
+import React from "react";
+const Lab = () => {
+  return (
+    <div className="container text-white bg-slate-600 min-w-full px-12 pb-32">
+      <h2 className="pt-3 text-3xl">Prácticas Laboratorio</h2>
+      <h5 className="pt-3 text-lg">Luis Abraham Meléndez Hernández</h5>
+      <h5 className="py-3 mb-4 text-lg">Ramón Alberto Romero Salazar</h5>
+      <div className="lg:mx-20">
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Páctica 1</th>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="practicas/Practica1.docx"
+                >
+                  Descargar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Páctica 2</th>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="practicas/Practica2.docx"
+                >
+                  Descargar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Páctica 3</th>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="practicas/Practica3.docx"
+                >
+                  Descargar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Páctica 4</th>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="practicas/Practica4.docx"
+                >
+                  Descargar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Páctica 5</th>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="practicas/Practica5.docx"
+                >
+                  Descargar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Páctica 7</th>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="practicas/Practica7.docx"
+                >
+                  Descargar
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Lab;
--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,58 +1,129 @@
-import React from 'react';
-import TableItem from './TableItem';
+import React from "react";
+import TableItem from "./TableItem";
 
 const Tareas = () => {
+  return (
+    <div className="container bg-slate-600 min-w-full px-12 pb-10">
+      <h1 className="pt-3 text-white text-3xl mb-4">Tareas</h1>
+      <div className="lg:mx-20">
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Hecha por</th>
+              <th scope="col">Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableItem num="1" alumno="Ramón" link="/tareas/1/Ramon" />
+            <tr>
+              <th scope="row">1</th>
+              <td>Karime</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea1Karime/index.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <TableItem num="2" alumno="Ramón" link="/tareas/2/Ramon" />
+            <tr>
+              <th scope="row">2</th>
+              <td>Karime</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea2Karime/index.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Ramon</td>
+              <td>
+                <a className="btn btn-error lg:btn-sm" href="tarea3Ramon.html">
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Karime</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea3Karime/index.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Ambos</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea4/Tarea4.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">5</th>
+              <td>Ambos</td>
+              <td>
+                <a className="btn btn-error lg:btn-sm" href="tarea5/index.html">
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">6</th>
+              <td>Ambos</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea6/Tarea6.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">7</th>
+              <td>Ambos</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea7/Tarea7.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">9</th>
+              <td>Ambos</td>
+              <td>
+                <a
+                  className="btn btn-error lg:btn-sm"
+                  href="tarea9/Tarea9.html"
+                >
+                  Tarea
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-    return ( 
-        <div className='container bg-slate-600 min-w-full px-12 pb-10'>
-            <h1 className='pt-3 text-white text-3xl mb-4'>Tareas</h1>
-            <div className='lg:mx-20'>
-                <table className="table table-dark table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Hecha por</th>
-                        <th scope="col">Link</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <TableItem num="1" alumno="Ramón" link="/tareas/1/Ramon"/>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Karime</td>
-                            <td><a className='btn btn-error lg:btn-sm' href="tarea1Karime/index.html">Tarea</a></td>
-                        </tr> 
-                        <TableItem num="2" alumno="Ramón" link="/tareas/2/Ramon"/>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Karime</td>
-                            <td><a className='btn btn-error lg:btn-sm' href="tarea2Karime/index.html">Tarea</a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Ramon</td>
-                            <td><a className='btn btn-error lg:btn-sm' href="tarea3Ramon.html">Tarea</a></td>
-                        </tr>   
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Karime</td>
-                            <td><a className='btn btn-error lg:btn-sm' href="tarea3Karime/index.html">Tarea</a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>Ambos</td>
-                            <td><a className='btn btn-error lg:btn-sm' href="tarea4/Tarea4.html">Tarea</a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">5</th>
-                            <td>Ambos</td>
-                            <td><a className='btn btn-error lg:btn-sm' href="tarea5/index.html">Tarea</a></td>
-                        </tr> 
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
-}
- 
 export default Tareas;
